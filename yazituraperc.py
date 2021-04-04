99d914beb4d846eb102127b61a30e081e028c0c8
balpa <44147280+balpa@users.noreply.github.com>
2021-04-04 15:35:42 +0300

    Add files via upload

---
+++ yazituraperc.py
@@ -0,0 +1,54 @@
+from datetime import time
+
+import matplotlib.pyplot as plt
+import numpy
+import random
+import time
+
+t0 = time.process_time()
+
+arr = ['yazi','tura']
+count = int(input("How many times you wanna flip?: "))
+saved, yazi, tura = [], [], []
+
+for x in range(count):
+    tot = random.choice(arr)
+    saved.append(tot)
+
+for y in range(len(saved)):
+    if saved[y] == 'yazi':
+        yazi.append(saved[y])
+    elif saved[y] == 'tura':
+        tura.append(saved[y])
+    else:
+        pass
+
+savedlen, yazilen, turalen = len(saved), len(yazi), len(tura)
+percyazi = yazilen*100/savedlen
+perctura = turalen*100/savedlen
+
+#print(saved)
+print(f"{savedlen} kere atıldı")
+print("Yazı gelenlerin sayısı: ",yazilen)
+print("Tura gelenlerin sayısı: ",turalen)
+print("Yazı gelenlerın oranı: "+str(percyazi)+"%")
+print("Tura gelenlerın oranı: "+str(perctura)+"%")
+
+names = ['YAZI ', 'TURA']
+values = [percyazi, perctura]
+plt.figure(figsize=(4, 4))
+bar = plt.bar(names, values)
+plt.title(f"{savedlen} kere atıldı")
+plt.ylabel("PERCENTAGE")
+
+for bar in bar:
+    yval = round(bar.get_height(), 2)
+    plt.text(bar.get_x() + .25, yval - 25, yval, fontsize=15)
+
+t1 = time.process_time()-t0
+print(f"{t1} seconds passed during execution.")
+
+plt.show()
+
+
+
